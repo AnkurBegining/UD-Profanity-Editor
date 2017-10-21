11171f47b7d6e24abd6bd285258b9b5c958ccbe8
AnkurBegining <201551016.iiitvadodara.ac.in@gmail.com>
2017-10-21 16:10:41 +0530

    feat:"file which need to read is opened"

---
+++ Profanity Editor/base.py
@@ -0,0 +1,6 @@
+
+def read_text():
+    quotes = open("/home/ankur/PycharmProjects/Profanity Editor/Profanity Editor/movie_quotes.txt")
+    content_of_file = quotes.read()
+    print(content_of_file)
+read_text()
