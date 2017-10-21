c138788b2da18172c83b12cce80f5f0c71a99d62
AnkurBegining <201551016.iiitvadodara.ac.in@gmail.com>
2017-10-21 18:31:43 +0530

    feat: "profanity checker added"

--- Profanity Editor/base.py
+++ Profanity Editor/base.py
@@ -1,6 +1,19 @@
+import urllib.request as ur
+import urllib.parse as parse
+
 
 def read_text():
     quotes = open("/home/ankur/PycharmProjects/Profanity Editor/Profanity Editor/movie_quotes.txt")
     content_of_file = quotes.read()
-    print(content_of_file)
+    quotes.close()
+    check_profanity(content_of_file)
+    #print(content_of_file)
+
+def check_profanity(text_to_be_checked):
+    quote = parse.quote_plus(text_to_be_checked)
+    connection = ur.urlopen("http://www.wdylike.appspot.com/?q="+quote)
+    output = connection.read()
+    print(output)
+    connection.close()
+
 read_text()
